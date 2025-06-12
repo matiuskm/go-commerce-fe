7a8caab6138a9cecad2a1767431d0189aae02840
lazycoder <matius.km@gmail.com>
2025-06-12 20:25:50 +0700

    refactor(auth): move AuthProvider from App to main.jsx
    Centralize authentication context provider at the root level for better structure and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import AuthProvider from "./context/AuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -25,7 +24,6 @@ import PaymentFailurePage from "./pages/PaymentFailed";
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <AuthProvider>
         <Header />
         <main className="flex-1">
           <Routes>
@@ -53,7 +51,6 @@ function App() {
           </Routes>
         </main>
         <Footer />
-      </AuthProvider>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import AuthProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,8 +12,10 @@ createRoot(document.getElementById('root')).render(
       v7_startTransition: true,
       v7_relativeSplatPath: true,
     }}>
-      <App />
-      <Toaster position="top-right" reverseOrder={false} />
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" reverseOrder={false} />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
